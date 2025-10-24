a987a195ef60b7ddc2bdcb9e09598169151304b3
Leonardo Estacio <mdnssknght@gmail.com>
2025-10-23 21:41:45 -0500

    Output to 12-bit per channel HEIC image

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,26 @@ use vulkano::{
     VulkanLibrary,
     buffer::{Buffer, BufferCreateInfo, BufferUsage},
     command_buffer::{
-        AutoCommandBufferBuilder, ClearColorImageInfo, CommandBufferUsage, CopyImageToBufferInfo,
+        AutoCommandBufferBuilder, ClearColorImageInfo, CommandBufferUsage,
         allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo},
     },
-    device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags},
+    descriptor_set::{
+        DescriptorSet, WriteDescriptorSet, allocator::StandardDescriptorSetAllocator,
+    },
+    device::{Device, DeviceCreateInfo, DeviceFeatures, QueueCreateInfo, QueueFlags},
     format::{ClearColorValue, Format},
-    image::{Image, ImageCreateInfo, ImageType, ImageUsage},
+    image::{Image, ImageCreateInfo, ImageType, ImageUsage, view::ImageView},
     instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
     memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator},
+    pipeline::{
+        ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout,
+        PipelineShaderStageCreateInfo, compute::ComputePipelineCreateInfo,
+        layout::PipelineDescriptorSetLayoutCreateInfo,
+    },
     sync::{self, GpuFuture},
 };
+
+mod shader;
 
 fn main() {
     let library = VulkanLibrary::new().expect("Failed to find local Vulkan library");
@@ -53,6 +63,12 @@ fn main() {
                 queue_family_index,
                 ..Default::default()
             }],
+            enabled_features: DeviceFeatures {
+                shader_float16: true,
+                shader_int16: true,
+                storage_buffer16_bit_access: true,
+                ..Default::default()
+            },
             ..Default::default()
         },
     )
@@ -65,23 +81,23 @@ fn main() {
     let width = 4000;
     let height = 3000;
 
-    let image = Image::new(
+    let rgb_image = Image::new(
         memory_allocator.clone(),
         ImageCreateInfo {
             image_type: ImageType::Dim2d,
-            format: Format::R16G16B16A16_UINT,
+            format: Format::R16G16B16A16_SFLOAT,
             extent: [width, height, 1],
-            usage: ImageUsage::TRANSFER_DST | ImageUsage::TRANSFER_SRC,
+            usage: ImageUsage::TRANSFER_DST | ImageUsage::STORAGE,
             ..Default::default()
         },
         AllocationCreateInfo::default(),
     )
     .unwrap();
 
-    let buffer = Buffer::from_iter(
+    let r_buffer = Buffer::from_iter(
         memory_allocator.clone(),
         BufferCreateInfo {
-            usage: BufferUsage::TRANSFER_DST,
+            usage: BufferUsage::STORAGE_BUFFER,
             ..Default::default()
         },
         AllocationCreateInfo {
@@ -89,7 +105,76 @@ fn main() {
                 | MemoryTypeFilter::HOST_RANDOM_ACCESS,
             ..Default::default()
         },
-        (0..width * height * 4 * 2).map(|_| 0u8),
+        (0..width * height * 2).map(|_| 0u8),
+    )
+    .unwrap();
+
+    let g_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        (0..width * height * 2).map(|_| 0u8),
+    )
+    .unwrap();
+
+    let b_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        (0..width * height * 2).map(|_| 0u8),
+    )
+    .unwrap();
+
+    let compute_shader = shader::finishing::load(device.clone())
+        .expect("Failed to create shader module")
+        .entry_point("main")
+        .unwrap();
+    let stage = PipelineShaderStageCreateInfo::new(compute_shader);
+    let layout = PipelineLayout::new(
+        device.clone(),
+        PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+            .into_pipeline_layout_create_info(device.clone())
+            .unwrap(),
+    )
+    .unwrap();
+
+    let compute_pipeline = ComputePipeline::new(
+        device.clone(),
+        None,
+        ComputePipelineCreateInfo::stage_layout(stage, layout),
+    )
+    .expect("Failed to create compute pipeline");
+
+    let descriptor_set_allocator = Arc::new(StandardDescriptorSetAllocator::new(
+        device.clone(),
+        Default::default(),
+    ));
+
+    let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+    let set = DescriptorSet::new(
+        descriptor_set_allocator.clone(),
+        layout.clone(),
+        [
+            WriteDescriptorSet::image_view(0, ImageView::new_default(rgb_image.clone()).unwrap()),
+            WriteDescriptorSet::buffer(1, r_buffer.clone()),
+            WriteDescriptorSet::buffer(2, g_buffer.clone()),
+            WriteDescriptorSet::buffer(3, b_buffer.clone()),
+        ],
+        [],
     )
     .unwrap();
 
@@ -105,18 +190,27 @@ fn main() {
     )
     .unwrap();
 
-    // Just clear color to purple
     command_buffer_builder
         .clear_color_image(ClearColorImageInfo {
-            clear_value: ClearColorValue::Uint([1000, 50, 1000, 4095]),
-            ..ClearColorImageInfo::image(image.clone())
+            clear_value: ClearColorValue::Float([1.0, 1.0, 1.0, 1.0]),
+            ..ClearColorImageInfo::image(rgb_image.clone())
         })
         .unwrap()
-        .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
-            image.clone(),
-            buffer.clone(),
-        ))
+        .bind_pipeline_compute(compute_pipeline.clone())
+        .unwrap()
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            0,
+            set,
+        )
         .unwrap();
+
+    unsafe {
+        command_buffer_builder
+            .dispatch([width / 8, height / 8, 1])
+            .unwrap();
+    }
 
     let command_buffer = command_buffer_builder.build().unwrap();
 
@@ -129,19 +223,31 @@ fn main() {
     future.wait(None).unwrap();
 
     let mut image =
-        libheif_rs::Image::new(width, height, ColorSpace::Rgb(RgbChroma::HdrRgbaLe)).unwrap();
+        libheif_rs::Image::new(width, height, ColorSpace::Rgb(RgbChroma::C444)).unwrap();
 
-    image
-        .create_plane(Channel::Interleaved, width, height, 12)
-        .unwrap();
+    image.create_plane(Channel::R, width, height, 12).unwrap();
+    image.create_plane(Channel::G, width, height, 12).unwrap();
+    image.create_plane(Channel::B, width, height, 12).unwrap();
 
     let planes = image.planes_mut();
 
-    let plane_interleaved = planes.interleaved.unwrap();
-
-    plane_interleaved
+    planes
+        .r
+        .unwrap()
         .data
-        .copy_from_slice(&*buffer.read().unwrap());
+        .copy_from_slice(&*r_buffer.read().unwrap());
+
+    planes
+        .g
+        .unwrap()
+        .data
+        .copy_from_slice(&*g_buffer.read().unwrap());
+
+    planes
+        .b
+        .unwrap()
+        .data
+        .copy_from_slice(&*b_buffer.read().unwrap());
 
     let lib_heif = LibHeif::new();
     let mut context = HeifContext::new().unwrap();
---
+++ src/shader.rs
@@ -0,0 +1,6 @@
+pub mod finishing {
+    vulkano_shaders::shader! {
+        ty: "compute",
+        path: "shader/finishing.glsl",
+    }
+}

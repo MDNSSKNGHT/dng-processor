a0e2a956bb584073f02a16365fe68200e7058553
Leonardo Estacio <mdnssknght@gmail.com>
2025-10-26 17:48:05 -0500

    Decode RAW image file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let raw_image = rawloader::decode_file("IMG_20251023_132747.dng").unwrap();
+    let raw_data = match raw_image.data {
+        rawloader::RawImageData::Integer(data) => data,
+        _ => panic!("Non-integer raw file processing is not supported"),
+    };
+
+    println!("{:?}", raw_data);
 }

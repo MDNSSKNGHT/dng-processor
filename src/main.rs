41fde25ef57dab0ab3498cd5d2e4fb6c4bb374ed
Leonardo Estacio <mdnssknght@gmail.com>
2025-10-26 21:35:59 -0500

    Demosaic RAW data using GPU

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,278 @@
+use std::sync::Arc;
+
+use vulkano::{
+    VulkanLibrary,
+    buffer::{Buffer, BufferCreateInfo, BufferUsage},
+    command_buffer::{
+        AutoCommandBufferBuilder, CommandBufferUsage, CopyBufferToImageInfo, CopyImageToBufferInfo,
+        PrimaryCommandBufferAbstract,
+        allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo},
+    },
+    descriptor_set::{
+        DescriptorSet, WriteDescriptorSet, allocator::StandardDescriptorSetAllocator,
+    },
+    device::{Device, DeviceCreateInfo, DeviceFeatures, Queue, QueueCreateInfo, QueueFlags},
+    format::Format,
+    image::{Image, ImageCreateInfo, ImageUsage, view::ImageView},
+    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
+    memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator},
+    pipeline::{
+        ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout,
+        PipelineShaderStageCreateInfo, compute::ComputePipelineCreateInfo,
+        layout::PipelineDescriptorSetLayoutCreateInfo,
+    },
+    sync::{self, GpuFuture},
+};
+
+fn setup_device_and_queue() -> (Arc<Device>, Arc<Queue>) {
+    let library = VulkanLibrary::new().expect("failed to find local Vulkan library");
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("failed to enumerate physical devices")
+        .next()
+        .unwrap();
+
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_, queue_family_properties)| {
+            queue_family_properties
+                .queue_flags
+                .contains(QueueFlags::COMPUTE)
+        })
+        .expect("failed to find a compute queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            enabled_features: DeviceFeatures {
+                shader_int16: true,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+
+    // I don't know how to return a Result, sorry
+    (device, queue)
+}
+
 fn main() {
-    let raw_image = rawloader::decode_file("IMG_20251023_132747.dng").unwrap();
-    let raw_data = match raw_image.data {
-        rawloader::RawImageData::Integer(data) => data,
-        _ => panic!("Non-integer raw file processing is not supported"),
+    let (device, queue) = setup_device_and_queue();
+
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+    let descriptor_set_allocator = Arc::new(StandardDescriptorSetAllocator::new(
+        device.clone(),
+        Default::default(),
+    ));
+    let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    ));
+
+    let (raw_image, width, height) = {
+        let image = rawloader::decode_file("IMG_20251023_132747.dng").unwrap();
+        let data = match image.data {
+            rawloader::RawImageData::Integer(data) => data,
+            _ => panic!("Non-integer raw file processing is not supported"),
+        };
+
+        let w = image.width as u32;
+        let h = image.height as u32;
+
+        let buffer = Buffer::from_iter(
+            memory_allocator.clone(),
+            BufferCreateInfo {
+                usage: BufferUsage::TRANSFER_SRC,
+                ..Default::default()
+            },
+            AllocationCreateInfo {
+                memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                    | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+                ..Default::default()
+            },
+            data,
+        )
+        .unwrap();
+
+        let image = Image::new(
+            memory_allocator.clone(),
+            ImageCreateInfo {
+                format: Format::R16_UINT,
+                extent: [w, h, 1],
+                usage: ImageUsage::STORAGE | ImageUsage::TRANSFER_DST,
+                ..Default::default()
+            },
+            AllocationCreateInfo::default(),
+        )
+        .unwrap();
+
+        let mut cbb = AutoCommandBufferBuilder::primary(
+            command_buffer_allocator.clone(),
+            queue.queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+
+        cbb.copy_buffer_to_image(CopyBufferToImageInfo::buffer_image(buffer, image.clone()))
+            .unwrap();
+
+        let cb = cbb.build().unwrap();
+
+        cb.execute(queue.clone())
+            .unwrap()
+            .then_signal_fence_and_flush()
+            .unwrap()
+            .wait(None /* timeout */)
+            .unwrap();
+
+        (image, w, h)
     };
 
-    println!("{:?}", raw_data);
+    let rgba_image = Image::new(
+        memory_allocator.clone(),
+        ImageCreateInfo {
+            format: Format::R16G16B16A16_UINT,
+            extent: raw_image.extent(),
+            usage: ImageUsage::STORAGE | ImageUsage::TRANSFER_SRC,
+            ..Default::default()
+        },
+        AllocationCreateInfo::default(),
+    )
+    .unwrap();
+
+    mod cs {
+        vulkano_shaders::shader! {
+            bytes: "spirv/demosaic.spirv"
+        }
+    }
+
+    let compute_shader = cs::load(device.clone())
+        .unwrap()
+        .entry_point("main")
+        .unwrap();
+    let stage = PipelineShaderStageCreateInfo::new(compute_shader);
+    let layout = PipelineLayout::new(
+        device.clone(),
+        PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+            .into_pipeline_layout_create_info(device.clone())
+            .unwrap(),
+    )
+    .unwrap();
+
+    let compute_pipeline = ComputePipeline::new(
+        device.clone(),
+        None,
+        ComputePipelineCreateInfo::stage_layout(stage, layout),
+    )
+    .expect("failed to create compute pipeline");
+
+    let raw_image_view = ImageView::new_default(raw_image.clone()).unwrap();
+    let rgba_image_view = ImageView::new_default(rgba_image.clone()).unwrap();
+
+    let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+    let set = DescriptorSet::new(
+        descriptor_set_allocator.clone(),
+        layout.clone(),
+        [
+            WriteDescriptorSet::image_view(0, raw_image_view),
+            WriteDescriptorSet::image_view(1, rgba_image_view),
+        ],
+        [],
+    )
+    .unwrap();
+
+    let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
+        command_buffer_allocator.clone(),
+        queue.queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .unwrap();
+
+    command_buffer_builder
+        .bind_pipeline_compute(compute_pipeline.clone())
+        .unwrap()
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            0,
+            set,
+        )
+        .unwrap();
+
+    unsafe {
+        command_buffer_builder
+            .dispatch(
+                [(width + 7) / 8, (height + 7) / 8, 1], /* rounding up */
+            )
+            .unwrap();
+    }
+    let command_buffer = command_buffer_builder.build().unwrap();
+
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+    future.wait(None).unwrap();
+
+    let rgba_buffer = {
+        let buffer = Buffer::from_iter(
+            memory_allocator.clone(),
+            BufferCreateInfo {
+                usage: BufferUsage::TRANSFER_DST,
+                ..Default::default()
+            },
+            AllocationCreateInfo {
+                memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                    | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+                ..Default::default()
+            },
+            (0..width * height * 4).map(|_| 0u16),
+        )
+        .unwrap();
+
+        let mut cbb = AutoCommandBufferBuilder::primary(
+            command_buffer_allocator.clone(),
+            queue.queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+
+        cbb.copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
+            rgba_image.clone(),
+            buffer.clone(),
+        ))
+        .unwrap();
+
+        let cb = cbb.build().unwrap();
+
+        cb.execute(queue.clone())
+            .unwrap()
+            .then_signal_fence_and_flush()
+            .unwrap()
+            .wait(None /* timeout */)
+            .unwrap();
+
+        buffer
+    };
+
+    println!("{:?}", rgba_buffer.read().unwrap());
 }
